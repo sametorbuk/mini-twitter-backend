9a533da33a357cc65286a79f506dcfb8805458bc
Samet Orbuk <orbuk60@gmail.com>
2024-12-26 19:49:40 +0300

    dockerfile setted and database connection done

---
+++ src/main/java/com/example/mini_twitter_backend/MiniTwitterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_twitter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTwitterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTwitterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_twitter_backend/MiniTwitterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_twitter_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTwitterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
